185cc444b37f4eab94c2233b2cc159e45d08c295
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-12-07 20:25:40 +0530

    New changes

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,21 +1,5 @@
-// import React from "react";
-// import { Hello } from "./Hello";
-
-// export const UserDetails = ({ userObj, hello }) => {
-//   return (
-//     <div>
-//       <div>
-//         <h1>{userObj.name}</h1>
-//         <h1>{userObj.age}</h1>
-//         <h1>{userObj.gender}</h1>
-//       </div>
-//       <Hello hello={hello} />
-//     </div>
-//   );
-// };
-
 import React from "react";
-import hello, { Hello } from "./Hello";
+import { Hello } from "./Hello";
 
 export const UserDetails = ({ userObj, hello }) => {
   return (
